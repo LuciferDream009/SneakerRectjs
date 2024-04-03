00258917f0226e3db7cab0a1822c111baa1641bc
Pham Van Thanh <88926531+thanhlucifer@users.noreply.github.com>
2024-04-03 14:48:38 +0700

    feat: create page, routes, components

---
+++ sneaker-app/src/routes/index.js
@@ -0,0 +1,28 @@
+import HomePage from "../pages/HomePage/HomePage";
+import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
+import OrderPage from "../pages/OrderPage/OrderPage";
+import ProductPage from "../pages/ProductPage/ProductPage";
+
+export const routes = [
+    {
+        path: "/",
+        page: HomePage,
+        isShowHeader: true
+    },
+    {
+        path: "/order",
+        page: OrderPage,
+        isShowHeader: true
+    },
+    {
+        path: "/product",
+        page: ProductPage,
+        isShowHeader: true
+    },
+    {
+        path: "*",
+        page: NotFoundPage
+
+    }
+
+]
