e4ad0ca00aafb1c37db84662ab3cec1ee0f9551b
Pham Van Thanh <88926531+thanhlucifer@users.noreply.github.com>
2024-04-09 19:16:16 +0700

    feat:  i react-query, test fetch api

--- sneaker-app/src/App.js
+++ sneaker-app/src/App.js
@@ -1,10 +1,25 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { routes } from './routes';
 import DefaultComponent from './components/DefaultComponent/DefaultComponent';
-
-
+import axios from 'axios';
+import {
+  useQuery
+} from '@tanstack/react-query'
 const App = () => {
+
+  // useEffect(() =>{
+  //   fetchApi()
+  // },[])
+  console.log('process.env.REACT_APP_API_URL', process.env.REACT_APP_API_URL)
+  const fetchApi = async () => {
+    const res = await axios.get(`${process.env.REACT_APP_API_URL}/product/getAll-product`)
+    return res.data
+  }
+
+    // Queries
+    const query = useQuery({ queryKey: ['todos'], queryFn: fetchApi })
+    console.log('query', query)
   return (
     <div>
     <Routes>
--- sneaker-app/src/index.js
+++ sneaker-app/src/index.js
@@ -6,16 +6,27 @@ import reportWebVitals from './reportWebVitals';
 import { store } from './redux/store'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom';
+import {
+  QueryClient,
+  QueryClientProvider
+} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+// Create a client
+const queryClient = new QueryClient()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+ // <React.StrictMode>
+ <QueryClientProvider client={queryClient}>
    <Provider store={store}>
    <BrowserRouter>
     <App />
   </BrowserRouter>
   </Provider>
-  </React.StrictMode>
+  <ReactQueryDevtools initialIsOpen={false} />
+</QueryClientProvider>
+//</React.StrictMode>
 );
 
 reportWebVitals();
